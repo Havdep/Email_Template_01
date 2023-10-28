f46dcf89fbf7d4d4cb3fc7d6820ac0a8fc043d76
Nipun <nipunsingh29@hotmail.com>
2023-10-28 18:08:48 +0530

    Refactor components and improve layout
    - Separated components and child components for better organization
    - Styled the layout to display types in a column and cards in rows
    - Fixed rendering of templates in CategoryList
    - Other minor improvements and clean-up

---
+++ src/comp/categoryList.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../Context";
+import "../styling/categoryList.css";
+import SearchBar from "./SearchBar"; // Corrected component name
+
+function CategoryList() {
+  const { templates, filterList, search, setSearch } =
+    useContext(GlobalContext);
+
+  // Filter the templates based on the selected filterList
+  const filteredTemplates = templates.filter((template) => {
+    const typeFilter =
+      filterList.length === 0 || filterList.includes(template.type);
+    const searchFilter = search
+      ? template.type.toLowerCase().includes(search.toLowerCase())
+      : true;
+    return typeFilter && searchFilter;
+  });
+  const groupedCards = filteredTemplates.reduce((groups, template) => {
+    if (!groups[template.type]) {
+      groups[template.type] = [];
+    }
+    groups[template.type].push(template);
+    return groups;
+  }, {});
+
+  return (
+    <div className="parent">
+      <SearchBar searchTerm={search} onSearch={setSearch} />
+
+      <div className="categories">
+        {Object.keys(groupedCards).map((type, index) => (
+          <div key={index} className="category">
+            <li className="category-name">{type}</li>
+            <div className="image-card">
+              {groupedCards[type].map((template, index) => (
+                <div key={index} className="card">
+                  <img
+                    src={template.imageUrl}
+                    className="image"
+                    alt={template.type}
+                  />
+                  <div className="temp-name">{template.name}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CategoryList;
--- src/components/GroupCrad.jsx
+++ src/components/GroupCrad.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../Context";
 import CategoryList from "./CategoryList";
+import "../styling/groupcard.css";
 
 function GroupCrad() {
+  const { templates } = useContext(GlobalContext);
+
+  // Group templates by type
+  const groupedTemplates = templates.reduce((groups, template) => {
+    if (!groups[template.type]) {
+      groups[template.type] = [];
+    }
+    groups[template.type].push(template);
+    return groups;
+  }, {});
+
   return (
-    <>
-      {/* <div>GroupCrad</div> */}
-      <CategoryList />
-    </>
+    <div className="container">
+      {Object.keys(groupedTemplates).map((type) => (
+        <div>
+          <div key={type} className="card-type">
+            <div>{type}</div>
+          </div>
+          <div className="card">
+            <CategoryList templates={groupedTemplates[type]} />
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
